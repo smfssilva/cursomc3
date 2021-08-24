bc8f0dd1d5f136992ec6a93950fa9e299fa699c4
Silva <smfssilva@yahoo.com>
2021-08-24 20:11:07 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/smfs/cursomc/resources/PedidoResource.java
+++ src/main/java/com/smfs/cursomc/resources/PedidoResource.java
@@ -18,8 +18,8 @@ public class PedidoResource {
 	private PedidoService servico;
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Pedido obj = servico.buscar(id);
+	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
+		Pedido obj = servico.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
 
