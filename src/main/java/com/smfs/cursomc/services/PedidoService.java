fc427cc2cbf7c16cb408862ff37a9f55787f7c4a
Silva <smfssilva@yahoo.com>
2021-08-24 00:44:48 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/smfs/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.smfs.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.smfs.cursomc.domain.Cliente;
+import com.smfs.cursomc.domain.Pedido;
+import com.smfs.cursomc.repositories.PedidoRepository;
+import com.smfs.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado Id: " + id
+				+ ", Tipo: " + Pedido.class.getName()));
+	}
+	
+}
