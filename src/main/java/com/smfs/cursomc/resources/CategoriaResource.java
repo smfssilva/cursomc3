6ae097dd17f68f24b83614e0fa838f65f36d5d27
Silva <smfssilva@yahoo.com>
2021-08-24 20:48:33 -0300

    Listando todas categorias com DTO

--- src/main/java/com/smfs/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/smfs/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,9 @@
 package com.smfs.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.smfs.cursomc.domain.Categoria;
+import com.smfs.cursomc.dto.CategoriaDTO;
 import com.smfs.cursomc.services.CategoriaService;
 
 @RestController
@@ -46,6 +50,13 @@ public class CategoriaResource {
     public ResponseEntity<Void> delete(@PathVariable Integer id){
     	service.delete(id);
     	return ResponseEntity.noContent().build();
+    }
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<CategoriaDTO>> findAll() {
+    	List<Categoria> list = service.findAll();
+    	List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+    	return ResponseEntity.ok().body(listDTO);
     }
     
 }
--- src/main/java/com/smfs/cursomc/services/CategoriaService.java
+++ src/main/java/com/smfs/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.smfs.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,4 +42,9 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
 	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+	
 }
